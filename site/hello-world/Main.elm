9e67f567d6ff83b76cfe862c610f6e774bc7c5af
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-26 00:11:10 -0500

    Make the hello world example use the beginnerProgram function

--- site/hello-world/Main.elm
+++ site/hello-world/Main.elm
@@ -1,8 +1,32 @@
-import Html exposing (div, p, text, button)
+import Html exposing (Html, div, p, text, button)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import Html.App exposing (beginnerProgram)
 
-main =
+main = beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model = String
+
+model: String
+model = "Hello World!"
+
+-- UPDATE
+
+type Msg = ChangeGreeting
+
+update : Msg -> Model -> Model
+update action model =
+  case action of
+    ChangeGreeting ->
+      "Goodbye Universe"
+
+-- VIEW
+
+view : Model -> Html Msg
+view model  =
   div [] [
-    p [] [text "Hello World!"],
-    button [class "btn btn-default"] [text "Click me!"]
+    p [] [text model],
+    button [ class "btn btn-default", onClick ChangeGreeting ] [text "Click me!"]
   ]
