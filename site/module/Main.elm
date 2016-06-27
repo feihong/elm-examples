5fedfb8782781e9e4595eed5caeb1841adbbe873
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-26 23:12:24 -0500

    Add a module example
    No modules in here just yet

---
+++ site/module/Main.elm
@@ -0,0 +1,61 @@
+import Html.App as App
+import Html exposing (Html, div, text)
+import Random
+import Char
+import String
+
+
+main = App.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions =  subscriptions
+  }
+
+
+hanzi : Random.Generator String
+hanzi = Random.map (\x -> x |> Char.fromCode |> String.fromChar) (Random.int 0x4e00 0x9fff)
+
+generateHanzi : Cmd Msg
+generateHanzi = Random.generate NewHanzi hanzi
+
+
+-- MODEL
+
+
+type alias Model = String
+
+
+init : (Model, Cmd Msg)
+init = ("", generateHanzi)
+
+
+-- UPDATE
+
+
+type Msg
+  = Generate
+  | NewHanzi String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Generate -> (model, generateHanzi)
+
+    NewHanzi value -> (value, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div [] [ text model ]
