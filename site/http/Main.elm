e1e197d114e02e2b4af0ae95750ad869a43c1ea9
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-26 22:06:16 -0500

    Add a button that grabs the Elm version from the decoded elm-package.json

--- site/http/Main.elm
+++ site/http/Main.elm
@@ -1,8 +1,10 @@
 import Html.App
 import Html exposing (Html, div, p, pre, text, button, code)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
 import Http
 import Task
+import Json.Decode as Decode
 
 
 main = Html.App.program
@@ -19,11 +21,12 @@ main = Html.App.program
 type alias Model =
   { content : String
   , errorMsg : String
+  , version : String
   }
 
 
 init : (Model, Cmd Msg)
-init = (Model "" "", fetch "elm-package.json")
+init = (Model "" "" "", fetch "elm-package.json")
 
 
 -- UPDATE
@@ -32,6 +35,8 @@ init = (Model "" "", fetch "elm-package.json")
 type Msg
   = Fetch String
   | Success String
+  | FetchJson String
+  | SuccessJson String
   | Failure Http.Error
 
 
@@ -43,6 +48,12 @@ update msg model =
 
     Success result ->
       ({ model | content = result }, Cmd.none)
+
+    FetchJson url ->
+      (model, fetchJson url)
+
+    SuccessJson result ->
+      ({ model | version = result }, Cmd.none)
 
     Failure error ->
       ({ model | errorMsg = toString error }, Cmd.none)
@@ -58,12 +69,20 @@ subscriptions model = Sub.none
 -- VIEW
 
 
+btnClass = class "btn btn-default"
+
+
 view : Model -> Html Msg
 view model =
   div []
     [ p [] [ text "Contents of this program's elm-package.json:" ]
     , pre [] [ text model.content ]
-    , button [ onClick (Fetch "does-not-exist.json") ] [ text "Make failing request" ]
+    , button [ btnClass, onClick (FetchJson "elm-package.json") ] [ text "Get Elm version" ]
+    , p []
+      [ text "Elm version: "
+      , code [] [ text model.version ]
+      ]
+    , button [ btnClass, onClick (Fetch "does-not-exist.json") ] [ text "Make failing request" ]
     , p []
       [ text "Error message:"
       , code [] [ text model.errorMsg ]
@@ -77,3 +96,11 @@ view model =
 fetch : String -> Cmd Msg
 fetch url =
   Task.perform Failure Success (Http.getString url)
+
+
+fetchJson : String -> Cmd Msg
+fetchJson url =
+  let
+    decodeElmVersion = Decode.at [ "elm-version" ] Decode.string
+  in
+    Task.perform Failure SuccessJson (Http.get decodeElmVersion url)
