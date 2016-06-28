e0ed67ece9173cb61c3d5d031b488b5cfeaf0961
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-28 15:37:46 -0500

    Add a form

--- site/bootstrap/Main.elm
+++ site/bootstrap/Main.elm
@@ -1,8 +1,17 @@
-import Html exposing (Html, div, p, text)
-import Html.Attributes exposing(class)
-import Bootstrap.Html exposing (row_, colSm_, colSmOffset_)
+import Html exposing (Html, div, p, text, form, label, input, button)
+import Html.Attributes exposing(class, type', name)
+import Html.Shorthand exposing (div_, h2_)
+import Bootstrap.Html exposing (row_, colSm_, colSmOffset_, formGroup_)
 
-main = div [ class "grid" ]
+main = div_
+  [ h2_ "Responsive grid system"
+  , grid
+  , h2_ "Forms"
+  , bootstrapForm
+  ]
+
+
+grid = div [ class "grid" ]
   [ row_
       [ colSm_ 6 2 [ text "6 2" ]
       , colSm_ 6 10 [ text "6 10"]
@@ -24,3 +33,29 @@ main = div [ class "grid" ]
       , colSm_ 6 2 [ text "6 2"]
       ]
   ]
+
+
+bootstrapForm = form []
+  [ formGroup_
+      [ label [] [ text "Username" ]
+      , input [ name "username", class "form-control" ] []
+      ]
+  , formGroup_
+      [ label [] [ text "Password" ]
+      , input [ name "password", class "form-control", type' "password" ] []
+      ]
+  , checkbox "coolio" "Check me out"
+  , btnDefault "submit" "Submit"
+  ]
+
+
+checkbox cbxName cbxText =
+  div [ class "checkbox" ]
+    [ label []
+      [ input [ name cbxName, type' "checkbox" ] []
+      , text cbxText
+      ]
+    ]
+
+btnDefault btnType label =
+  button [ class "btn btn-default", type' btnType ] [ text label ]
