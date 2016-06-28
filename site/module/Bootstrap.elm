3dd07d7cbe7dcadffb55379c8a90126dcff81f6e
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-27 22:25:11 -0500

    Add type annotations

--- site/module/Bootstrap.elm
+++ site/module/Bootstrap.elm
@@ -4,9 +4,11 @@ import Html as Htm
 import Html.Attributes exposing (class, type', placeholder)
 
 
+button : String -> Htm.Html a
 button title =
   Htm.button [ class "btn btn-default" ] [ Htm.text title ]
 
 
+input : String -> Htm.Html a
 input placeHolder =
   Htm.input [ type' "text", class "form-control", placeholder placeHolder ] []
