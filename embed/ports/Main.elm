5d7862e7ff78811467fab12f0c7eb6a5b78966b8
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-29 21:27:08 -0500

    Use flags to pass data from JS to Elm

--- embed/ports/Main.elm
+++ embed/ports/Main.elm
@@ -1,7 +1,52 @@
 module Main exposing (..)
 
-import Html exposing (..)
+import Html exposing (Html, div, p, em, strong, text)
+
+
+type alias Flags =
+    { speechSynthesisSupported : Bool }
 
 
 main =
-    text "Let us begin..."
+    Html.programWithFlags
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type alias Model =
+    { speechSynthesisSupported : Bool }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { speechSynthesisSupported = flags.speechSynthesisSupported }, Cmd.none )
+
+
+type Msg
+    = None
+
+
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ supportView model ]
+
+
+supportView : Model -> Html Msg
+supportView model =
+    let
+        ( tag, msg ) =
+            if model.speechSynthesisSupported then
+                ( em, "Speech synthesis is supported." )
+            else
+                ( strong, "Warning: speech synthesis is not supported!" )
+    in
+        p []
+            [ tag [] [ text msg ] ]
