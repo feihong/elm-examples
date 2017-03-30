9c7ad0f168bd1cbfed64d296f3a3d11d6c08597f
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-29 22:04:51 -0500

    Set up port that allows Elm to speak

--- embed/ports/Main.elm
+++ embed/ports/Main.elm
@@ -1,12 +1,18 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, div, p, em, strong, text)
+import Html exposing (Html, div, p, em, strong, text, button, input)
+import Html.Attributes exposing (value, size, autofocus)
+import Html.Events exposing (onClick, onInput)
+
+
+port speak : String -> Cmd msg
 
 
 type alias Flags =
     { speechSynthesisSupported : Bool }
 
 
+main : Program Flags Model Msg
 main =
     Html.programWithFlags
         { init = init
@@ -17,26 +23,50 @@ main =
 
 
 type alias Model =
-    { speechSynthesisSupported : Bool }
+    { speechSynthesisSupported : Bool
+    , text : String
+    }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { speechSynthesisSupported = flags.speechSynthesisSupported }, Cmd.none )
+    ( { speechSynthesisSupported = flags.speechSynthesisSupported
+      , text = "What am  I doing here?"
+      }
+    , Cmd.none
+    )
 
 
 type Msg
-    = None
+    = Speak
+    | ChangeText String
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Speak ->
+            ( model, speak model.text )
+
+        ChangeText text ->
+            ( { model | text = text }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ supportView model ]
+        [ supportView model
+        , div []
+            [ input
+                [ value model.text
+                , onInput ChangeText
+                , size 40
+                , autofocus True
+                ]
+                []
+            , button [ onClick Speak ] [ text "Speak" ]
+            ]
+        ]
 
 
 supportView : Model -> Html Msg
