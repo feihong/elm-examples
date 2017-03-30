affa706cc3d03b27d48e8f45f8c43f7bb3ed1b03
Feihong <feihong.hsu@gmail.com>
2017-03-30 18:15:32 -0500

    Use a promise to wait for voices to be available

--- embed/ports/main.js
+++ embed/ports/main.js
@@ -1,29 +1,34 @@
-let isSupported = window.speechSynthesis !== undefined
-if (isSupported) {
-    let voices = speechSynthesis.getVoices()
-    // If voices is empty, we need to wait for them to be loaded.
-    if (voices.length === 0) {
-        window.speechSynthesis.onvoiceschanged = init
-    } else {
-        init()
-    }
-} else {
-    init()
+function getVoices() {
+    return new Promise(resolve => {
+        let isSupported = window.speechSynthesis !== undefined
+        if (!isSupported) {
+            resolve(null)
+            return
+        }
+
+        let voices = speechSynthesis.getVoices()
+        if (voices.length) {
+            resolve(voices)
+        } else {
+            speechSynthesis.onvoiceschanged = () => {
+                resolve(speechSynthesis.getVoices())
+            }
+        }
+    })
 }
 
-function init() {
-    let voices = isSupported ? speechSynthesis.getVoices() : []
-
-    let app = Elm.Main.embed(document.getElementById('main'), {
-        isSupported: isSupported,
+getVoices().then(voices => {
+    let node = document.getElementById('main')
+    let app = Elm.Main.embed(node, {
+        isSupported: voices !== null,
         initialText: "Hey hey we're the Monkees, and we like to monkey around",
-        voices: voices
+        voices: voices || []
     })
 
     app.ports.speak.subscribe(function(text) {
         let utterance = new SpeechSynthesisUtterance(text)
-        utterance.onstart = function() { app.ports.speechStatus.send('start') }
-        utterance.onend = function() { app.ports.speechStatus.send('end') }
+        utterance.onstart = () => app.ports.speechStatus.send('start')
+        utterance.onend = () => app.ports.speechStatus.send('end')
         speechSynthesis.speak(utterance)
     })
-}
+})
