b9f6f718f6620c65887b882b7a8369d62dcf1946
Feihong <feihong.hsu@gmail.com>
2017-03-31 19:36:26 -0500

    Add more Json koans

--- simple/JsonKoans.elm
+++ simple/JsonKoans.elm
@@ -13,26 +13,30 @@ tests =
         [ test "decodeString" <|
             \() ->
                 let
-                    value =
+                    json =
                         """["monkey", "piano", "sound"]"""
                 in
-                    decodeString (list string) value
+                    decodeString (list string) json
                         |> Expect.equal (Ok [ "monkey", "piano", "sound" ])
         , test "decodeString" <|
             \() ->
                 let
-                    value =
+                    json =
                         """{"a": 1.2, "b": 2.3, "c": 3.4}"""
 
                     expectedDict =
                         Dict.fromList [ ( "a", 1.2 ), ( "b", 2.3 ), ( "c", 3.4 ) ]
                 in
-                    decodeString (dict float) value
+                    decodeString (dict float) json
                         |> Expect.equal (Ok expectedDict)
         , test "field" <|
             \() ->
-                decodeString (field "z" int) """{"z": 888}"""
-                    |> Expect.equal (Ok 888)
+                let
+                    json =
+                        """{"x": 1, "y": 2, "z": 888}"""
+                in
+                    decodeString (field "z" int) json
+                        |> Expect.equal (Ok 888)
         , test "at" <|
             \() ->
                 let
@@ -47,6 +51,43 @@ tests =
                 in
                     ( role, level )
                         |> Expect.equal ( Ok "paladin", Ok 9001 )
+        , test "index" <|
+            \() ->
+                let
+                    json =
+                        """[1, 2, 3, 4, 5, 6, 7, 8]"""
+                in
+                    List.range 0 7
+                        |> List.map (\i -> decodeString (index i int) json)
+                        |> Expect.equal
+                            (List.map Ok [ 1, 2, 3, 4, 5, 6, 7, 8 ])
+        , test "oneOf" <|
+            \() ->
+                let
+                    json =
+                        """[1,2,null,4]"""
+
+                    badInt =
+                        oneOf [ int, null -1 ]
+                in
+                    decodeString (list badInt) json
+                        |> Expect.equal (Ok [ 1, 2, -1, 4 ])
+        , test "map" <|
+            \() ->
+                let
+                    json =
+                        "\"A112-44Z\""
+
+                    splitId s =
+                        case String.split "-" s of
+                            a :: b :: _ ->
+                                ( a, b )
+
+                            _ ->
+                                ( "", "" )
+                in
+                    decodeString (map splitId string) json
+                        |> Expect.equal (Ok ( "A112", "44Z" ))
         ]
 
 
