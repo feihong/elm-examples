45d52ec880b22e1c70ac539b087d2e2df68e90ea
Feihong Hsu <feihong.hsu@gmail.com>
2017-04-13 16:27:19 -0500

    Add tests for centsToString

--- embed/dinner-calc/tests/TestHelpers.elm
+++ embed/dinner-calc/tests/TestHelpers.elm
@@ -3,12 +3,28 @@ module TestHelpers exposing (..)
 import Test exposing (..)
 import Expect
 import Test.Runner.Html as Runner
+import Helpers exposing (..)
 
 
 tests : Test
 tests =
     describe "Helper tests"
-        [ test "woo" <|
-            \() ->
-                True |> Expect.equal True
+        [ describe "centsToString"
+            [ test "0" <|
+                \() ->
+                    centsToString 0
+                        |> Expect.equal "0.00"
+            , test "300" <|
+                \() ->
+                    centsToString 300
+                        |> Expect.equal "3.00"
+            , test "503" <|
+                \() ->
+                    centsToString 503
+                        |> Expect.equal "5.03"
+            , test "1136" <|
+                \() ->
+                    centsToString 1136
+                        |> Expect.equal "11.36"
+            ]
         ]
