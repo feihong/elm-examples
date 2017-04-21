fc60c87170fa3a112d53789f1a5632ac2379e4f6
Feihong Hsu <feihong.hsu@gmail.com>
2017-04-20 20:10:41 -0500

    Add more shapes

--- simple/GraphicsExample.elm
+++ simple/GraphicsExample.elm
@@ -8,7 +8,7 @@ module Main exposing (..)
 import Color exposing (..)
 import Html exposing (Html)
 import Html.Attributes exposing (style)
-import Collage exposing (collage, ngon, filled, move)
+import Collage exposing (..)
 import Element exposing (Element)
 
 
@@ -16,27 +16,39 @@ main : Html msg
 main =
     Html.div
         [ style
-            [ ( "padding", "2rem" )
+            [ ( "margin", "2rem" )
             , ( "border", "1px dashed gray" )
+            , ( "display", "inline-block" )
             ]
         ]
-        [ view |> Element.toHtml
-        ]
+        [ view |> Element.toHtml ]
 
 
 view : Element
 view =
     collage 300
         300
-        [ ngon 4 75
+        [ ngon 6 75
             |> filled red
-            |> move ( -10, 0 )
-        , ngon 5 50
+            |> move ( -30, 0 )
+        , circle 50
             |> filled clearGrey
             |> move ( 50, 10 )
+        , circle 20
+            |> filled niceBlue
+            |> move ( 0, 120 )
+        , ngon 3 50
+            |> filled purple
+            |> move ( -10, -100 )
+            |> rotate (degrees 15)
         ]
 
 
 clearGrey : Color
 clearGrey =
     rgba 111 111 111 0.6
+
+
+niceBlue : Color
+niceBlue =
+    (hsl (degrees 206) 0.57 0.63)
