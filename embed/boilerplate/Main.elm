d541299680c9dfed43bc584be74317690b81d3c7
Feihong Hsu <feihong.hsu@gmail.com>
2017-04-30 15:13:57 -0500

    Add elm embed boilerplate

---
+++ embed/boilerplate/Main.elm
@@ -0,0 +1,62 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Json.Decode as Decode
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , powerLevel : Int
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    { name = "Coolio"
+    , powerLevel = 9001
+    }
+        ! []
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model.name
+        , text " has power level "
+        , strong [] [ text <| toString model.powerLevel ]
+        ]
